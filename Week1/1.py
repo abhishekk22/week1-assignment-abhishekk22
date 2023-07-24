eaa6db72b3d02cfec1ff60fad760bc267214c4ca
abhishekk22 <128130033+abhishekk22@users.noreply.github.com>
2023-07-24 10:38:53 +0530

    Add files via upload

---
+++ Week1/1.py
@@ -0,0 +1,8 @@
+# write python script to enter any number and check it is prime or not
+
+a=int(input('Enter any number:'))
+if(a%1 & a%a):
+    print("prime number")
+else:
+    print("not prime number")
+
